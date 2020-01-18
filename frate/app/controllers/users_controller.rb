61e127ce3fa3f30a37ea48897386a30ba2b59d7e
coronaisme <coronaisme@gmail.com>
2020-01-17 16:11:40 -0800

    users_controller

--- frate/app/controllers/users_controller.rb
+++ frate/app/controllers/users_controller.rb
@@ -1,2 +1,39 @@
 class UsersController < ApplicationController
+  def index 
+    @users = User.all
+  end
+
+  def new 
+    @user = User.new 
+  end
+
+  def create 
+    @user = User.new(user_params)
+    if @user.save 
+      redirect_to @user 
+    else 
+      render :new
+    end
+  end
+
+  def show 
+    @user = User.find(params[:id])
+    @friends = @user.friends 
+    @friendes = @user.friendes 
+    #@not_friending = @user.not_friending
+  end
+
+  def update
+    @user = User.find(params[:id])
+    User.find_by(user_params).followers << @user
+    redirect_to '/users/#{@user.id}'
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :age, :bio, :food_choice, :love_choice)
+  end
+  
 end
