9ee1c8765d6d45f78b153bf8065c9be97fad8c67
coronaisme <coronaisme@gmail.com>
2020-01-22 09:50:43 -0800

    merging

--- frate/app/controllers/reviews_controller.rb
+++ frate/app/controllers/reviews_controller.rb
@@ -1,7 +1,21 @@
 class ReviewsController < ApplicationController
 
-    def show
-        @user = User.find(params[:id])
+    def index  #all reviews associated with a ceratin user
+        @review = Review.all
+    end
+
+    def show # a single review? How/why do we want this
+        @review = Review.find(params[:id])
+    end
+
+    def new
+        @review = Review.new
+    end
+
+    #save with strong params ?
+    def create 
+        @review = Review.new 
+        #if @review.save ...
     end
 
     
