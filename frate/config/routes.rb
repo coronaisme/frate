21fc317e7709c741056f305cb1b02e63d2c488c3
coronaisme <coronaisme@gmail.com>
2020-01-21 16:45:55 -0800

    added show functionality to discern user

--- frate/config/routes.rb
+++ frate/config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
 
   get 'login', to: 'sessions#new'
-  post 'login', to: 'sessions#create'
+  post '/login', to: 'sessions#create'
   get 'authorized', to: 'sessions#page_requires_login'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
