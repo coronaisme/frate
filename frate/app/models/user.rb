0e9f5fca63ee9d3c4576dec54f8a2435869e839b
coronaisme <coronaisme@gmail.com>
2020-01-23 09:32:30 -0800

    merging

--- frate/app/models/user.rb
+++ frate/app/models/user.rb
@@ -12,6 +12,9 @@ class User < ApplicationRecord
 
     has_secure_password
     
+    has_many :reviews
+    has_one_attached :avatar
+    validates :name, presence: true, uniqueness: true
   
 
     def not_friends
